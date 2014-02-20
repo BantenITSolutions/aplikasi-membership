f93bcaff226bc815a9ee864da823fb969c2f793e
Endy Muhardin <endy.muhardin@gmail.com>
2014-02-20 20:57:17 +0700

    gunakan redis instance langsung dari redistogo, tidak melalui heroku

--- index.js
+++ index.js
@@ -10,13 +10,11 @@ http.createServer(function(request, response){
 
 console.log("App ready at port "+port);
 
-if (process.env.REDISTOGO_URL) {
-    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-    var redis = require("redis").createClient(rtg.port, rtg.hostname);
-    redis.client.auth(rtg.auth.split(":")[1]);
-} else {
-    var redis = require("redis").createClient();
-}
+var REDISTOGO_URL = "redis://redistogo:8adc4536655ce179e2645ae4abd70eb2@pearlfish.redistogo.com:9930/";
+
+var rtg   = require("url").parse(REDISTOGO_URL);
+var redis = require("redis").createClient(rtg.port, rtg.hostname);
+redis.auth(rtg.auth.split(":")[1]);
 
 var displayContent = function(){
     redis.get("nama", function(err, reply){
