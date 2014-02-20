f453c8fe6b48e2ac114d87b95f5dc0f0cb7e38f1
Endy Muhardin <endy.muhardin@gmail.com>
2014-02-20 16:51:53 +0700

    implement query ke redis

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ http.createServer(function(request, response){
 }).listen(port);
 
 console.log("App ready at port "+port);
+
+var redis = require("redis").createClient();
+
+var displayContent = function(){
+    redis.get("nama", function(err, reply){
+        console.log("Nama : "+reply);
+        redis.expire("nama", 1);    
+    });
+
+    redis.get("email", function(err, reply){
+        console.log("Email : "+reply);
+        redis.expire("email", 1);    
+    });
+};
+
+redis.on("connect", function(){
+    redis.set("nama", "Endy Muhardin");
+    redis.set("email", "endy.muhardin@gmail.com");
+    displayContent();
+});
